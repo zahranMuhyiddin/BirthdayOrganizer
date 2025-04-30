e779675208b1161ba0b995a2f54beba02ea2ad1f
ZEEd31 <zmzmnknk@gmail.com>
2025-04-30 08:30:24 +0700

    progres lanjutan

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+}
+
+// klik menghilangkan menu side bar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) || navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+})
+
+
+
+
+
+
+// jawaban faq
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
